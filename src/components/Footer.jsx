45e0191371ad1e34f889279e66d4414df9d21c77
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-02-25 12:54:34 +0530

    Initial build 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { RiGithubFill, RiInstagramFill, RiTwitterFill } from "@remixicon/react";
 
 function Footer() {
   return (
-    <div className="w-full bg-black lg:h-[45vh] h-[30vh] flex flex-col justify-center items-center gap-[1.4vw] px-[10vw] py-[1.3vw]">
-      <p className="helvetica italic text-[1.2vw] text-white">
+    <div className="w-full bg-black lg:h-[45vh] h-[32vh] flex flex-col justify-center items-center gap-[1.4vw] px-[10vw] py-[1.3vw]">
+      <p className="helvetica italic text-[1.8vw] lg:text-[1.2vw] text-white">
         Design Inspiration taken from Lera Zhurbenko
       </p>
       <div className="w-full links flex justify-center items-center gap-[2vw]">
@@ -36,14 +36,14 @@ function Footer() {
           <RiInstagramFill className="text-white" size={36} />
         </a>
       </div>
-      <p className="text-white helvetica italic">
+      <p className="text-white text-justify helvetica italic">
         <span className="font-bold">Disclaimer:</span> This website, inspired by
         a Behance project, is for educational purposes. The design and images
         used belong to their respective owners. I neither claim ownership nor
         intend to profit from them. If any concerns arise, kindly contact me.
         Thank you for understanding.
       </p>
-      <p className="text-white text-[1.2vw] helvetica">
+      <p className="text-white text-[1.8vw] lg:text-[1.2vw] helvetica">
         Developed with &#x2764; | Nila Laishram | &#169;2024 | All Rights
         Reserved
       </p>
